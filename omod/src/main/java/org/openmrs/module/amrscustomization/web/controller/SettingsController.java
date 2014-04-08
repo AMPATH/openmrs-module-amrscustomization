f2dce721bcd7526148e9da72ce951b7187f5cad0
mhawila <a.mhawila@gmail.com>
2014-04-08 10:18:20 +0300

    Renamed SettingsController bean to amrsCustomizationSettings to avoid conflict with core

--- omod/src/main/java/org/openmrs/module/amrscustomization/web/controller/SettingsController.java
+++ omod/src/main/java/org/openmrs/module/amrscustomization/web/controller/SettingsController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
  *
  * @author jkeiper
  */
-@Controller
+@Controller("amrsCustomizationSettings")
 @RequestMapping("module/amrscustomization/settings.htm")
 public class SettingsController {
 
